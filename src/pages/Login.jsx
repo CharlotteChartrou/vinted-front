4b22f82dbe0abc8ea6799bb9cb6dc508adf0fa0b
Chachou <chachou@mbp-de-chachou.lan>
2023-08-24 14:51:14 +0200

    step 2 sign up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,12 +4,15 @@ const Login = () => {
   return (
     <div className="container">
       <div className="form">
+        <h2>Se connecter</h2>
         <form className="login">
           <input placeholder="Adresse mail" type="email"></input>
           <input placeholder="Mot de passe" type="password"></input>
           <button>Se Connecter</button>
         </form>
-        <Link to="/signup">Pas encore de compte ? Inscris-toi !</Link>
+        <Link to="/signup" style={{ textDecoration: "none", color: "#007783" }}>
+          Pas encore de compte ? Inscris-toi !
+        </Link>
       </div>
     </div>
   );
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,16 +5,35 @@ import Cookies from "js-cookie";
 
 const Signup = () => {
   const [data, setData] = useState(Cookies.get("token") || null);
+  const [newUser, setNewUser] = useState({ name: "", email: "", password: "" });
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  const handleInput = (event) => {
+    setNewUser({ ...newUser, [event.target.name]: event.target.event });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Pour empêcher le navigateur de changer de page lors de la soumission du formulaire
+    axios
+      .post("https://lereacteur-vinted-api.herokuapp.com/user/signup", {
+        newUser,
+      })
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
+
+  /*   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.post(
-          "https://lereacteur-vinted-api.herokuapp.com/user/login"
+        const response = await 
+          {
+            name: name,
+            email: email,
+            password: password,
+          }
         );
         setData(response.data);
-        console.log(response.data);
+        console.log(response);
         setIsLoading(false);
       } catch (error) {
         console.log(error.message);
@@ -22,30 +41,49 @@ const Signup = () => {
     };
 
     fetchData();
-  }, []);
+  }, []); */
 
-  return isLoading ? (
-    <p>Loading...</p>
-  ) : (
+  /*   return isLoading ? (
+    <p>Loading</p>
+  ) : */ return (
     <div className="container">
       <div className="form">
-        <h1>S'inscrire</h1>
+        <h2>S'inscrire</h2>
         <form className="signup" onSubmit={handleSubmit}>
-          <input placeholder="Nom d'utilisateur" type="text" />
-          <input placeholder="Email" type="email" />
-          <input placeholder="Mot de passe" type="password" />
-          <span className="check-box">
-            <input type="checkbox" name="price"></input>{" "}
-            <span>S'inscrire à notre newsletter</span>
-            <div>
-              En m'inscrivant je confirme avoir lu et accepté les Termes &
-              Conditions et Politique de Confidentialité de Vinted. Je confirme
-              avoir au moins 18 ans.
-            </div>
-          </span>
+          <input
+            placeholder="Nom d'utilisateur"
+            type="text"
+            name="name"
+            onChange={handleInput}
+          />
+          <input
+            placeholder="Email"
+            type="email"
+            name="email"
+            onChange={handleInput}
+          />
+          <input
+            placeholder="Mot de passe"
+            type="password"
+            name="password"
+            onChange={handleInput}
+          />
+          <span className="checkbox-container">
+            <span className="checkbox" style={{ color: "#666" }}>
+              <input type="checkbox" name="price"></input> S'inscrire à notre
+              newsletter
+            </span>
+          </span>{" "}
+          <div style={{ color: "#666" }}>
+            En m'inscrivant je confirme avoir lu et accepté les Termes &
+            Conditions et Politique de Confidentialité de Vinted. Je confirme
+            avoir au moins 18 ans.
+          </div>
           <button type="submit">S'inscrire</button>
         </form>
-        <Link to="/login">Tu as déjà un compte ? Connecte-toi !</Link>
+        <Link style={{ textDecoration: "none", color: "#007783" }} to="/login">
+          Tu as déjà un compte ? Connecte-toi !
+        </Link>
       </div>
     </div>
   );
