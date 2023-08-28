d2587bc368d9eac6b42b7ffc221715158547812e
Chachou <chachou@mbp-de-chachou.lan>
2023-08-28 14:31:16 +0200

    publish css

--- src/pages/Publish.jsx
+++ src/pages/Publish.jsx
@@ -16,118 +16,149 @@ const Publish = ({ token }) => {
 
   return (
     <>
-      <h2>Vends ton article</h2>
-      <form
-        className="post"
-        onSubmit={async (event) => {
-          event.preventDefault();
-          try {
-            const formData = new FormData();
+      <div className="container">
+        <h2>Vends ton article</h2>
+        <form
+          className="post"
+          onSubmit={async (event) => {
+            event.preventDefault();
+            try {
+              const formData = new FormData();
 
-            /*      formData.append("user", user); */
-            formData.append("picture", picture);
-            formData.append("title", title);
-            formData.append("description", description);
-            formData.append("price", price);
-            formData.append("brand", brand);
-            formData.append("condistion", condition);
-            formData.append("city", city);
-            formData.append("size", size);
-            formData.append("color", color);
+              /*      formData.append("user", user); */
+              formData.append("picture", picture);
+              formData.append("title", title);
+              formData.append("description", description);
+              formData.append("price", price);
+              formData.append("brand", brand);
+              formData.append("condistion", condition);
+              formData.append("city", city);
+              formData.append("size", size);
+              formData.append("color", color);
 
-            const response = await axios.post(
-              "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
-              formData,
+              const response = await axios.post(
+                "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
+                formData,
 
-              {
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                  "Content-Type": "multipart/form-data",
-                },
-              }
-            );
-            console.log(formData);
-            setImgFromCloudinary(response.data.secure_url);
-          } catch (error) {
-            console.log(error.message);
-          }
-        }}
-      >
-        <input
-          type="file"
-          onChange={(event) => {
-            setPicture(event.target.files[0]);
+                {
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "multipart/form-data",
+                  },
+                }
+              );
+              console.log(formData);
+              setImgFromCloudinary(response.data.secure_url);
+            } catch (error) {
+              console.log(error.response);
+            }
           }}
-        />
-        <input
-          type="text"
-          placeholder="ex: Chemise Sézanne verte"
-          value={title}
-          onChange={(event) => {
-            setTitle(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="ex: porté quelquefois, taille correctement"
-          value={description}
-          onChange={(event) => {
-            setDescription(event.target.value);
-          }}
-        />
+        >
+          <div className="file-select">
+            <input
+              type="file"
+              onChange={(event) => {
+                setPicture(event.target.files[0]);
+              }}
+            />
+          </div>
 
-        <input
-          type="text"
-          placeholder="ex: Zara"
-          value={brand}
-          onChange={(event) => {
-            setBrand(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="ex: L / 40 / 12"
-          value={size}
-          onChange={(event) => {
-            setSize(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="ex: Fushia"
-          value={color}
-          onChange={(event) => {
-            setColor(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Neuf avec étiquettes"
-          value={condition}
-          onChange={(event) => {
-            setCondition(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="ex: Paris"
-          value={city}
-          onChange={(event) => {
-            setCity(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="0,00€"
-          value={price}
-          onChange={(event) => {
-            setPrice(event.target.value);
-          }}
-        />
-        <input type="checkbox" name="checkbox"></input>
-        <input type="submit" value="Ajouter" />
-      </form>
-      {imgFromCloudinary && <img src={imgFromCloudinary} alt="" />}
+          <div className="text-input-section">
+            <div className="text-input">
+              <h4>Titre</h4>
+              <input
+                type="text"
+                placeholder="ex: Chemise Sézanne verte"
+                value={title}
+                onChange={(event) => {
+                  setTitle(event.target.value);
+                }}
+              />
+            </div>
+            <div className="text-input">
+              <h4>Décris ton article</h4>
+              <input
+                type="text"
+                placeholder="ex: porté quelquefois, taille correctement"
+                value={description}
+                onChange={(event) => {
+                  setDescription(event.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className="text-input-section">
+            <div className="text-input">
+              <h4>Marque</h4>
+              <input
+                type="text"
+                placeholder="ex: Zara"
+                value={brand}
+                onChange={(event) => {
+                  setBrand(event.target.value);
+                }}
+              />
+            </div>
+            <div className="text-input">
+              <h4>Taille</h4>
+              <input
+                type="text"
+                placeholder="ex: L / 40 / 12"
+                value={size}
+                onChange={(event) => {
+                  setSize(event.target.value);
+                }}
+              />
+            </div>
+            <div className="text-input">
+              <h4>Couleur</h4>
+              <input
+                type="text"
+                placeholder="ex: Fushia"
+                value={color}
+                onChange={(event) => {
+                  setColor(event.target.value);
+                }}
+              />
+            </div>
+            <div className="text-input">
+              <h4>Etat</h4>
+              <input
+                type="text"
+                placeholder="Neuf avec étiquettes"
+                value={condition}
+                onChange={(event) => {
+                  setCondition(event.target.value);
+                }}
+              />
+            </div>
+            <div className="text-input">
+              <h4>Lieu</h4>
+              <input
+                type="text"
+                placeholder="ex: Paris"
+                value={city}
+                onChange={(event) => {
+                  setCity(event.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className="text-input-section">
+            <h4>Prix</h4>
+            <input
+              type="text"
+              placeholder="0,00€"
+              value={price}
+              onChange={(event) => {
+                setPrice(event.target.value);
+              }}
+            />
+            <input type="checkbox" name="checkbox"></input>
+          </div>
+          <input type="submit" value="Ajouter" />
+        </form>
+      </div>
     </>
   );
 };
